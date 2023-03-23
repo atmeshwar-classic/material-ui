1356af4eb9e7055c166f069dd22c25ee96f4fd7c
anmol <anmol.arora@classicInformatics.com>
2023-03-23 20:26:06 +0530

    bottom navigation done -  routing of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import AppRoutes from "./appRoutes/AppRoutes";
+import BottomNav from "./components/BottomNav/BottomNav";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <AppRoutes />
+      <BottomNav />
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@emotion/react";
 import { myTheme } from "./themes/globalTheme";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider theme={myTheme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </ThemeProvider>
 );
