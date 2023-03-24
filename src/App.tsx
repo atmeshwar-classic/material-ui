38865775c3f9e9f1f3da5deaee6f7a39428ccc15
RahulClassicInformatics <rahul.chaudhary@classicinformatics.com>
2023-03-24 17:14:50 +0530

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import HomePage from './Home/Home';
 import Navbar from './Navbar/Navabr';
 import Page2 from './Page2/Page2';
 import Page3 from './Page3/Page3';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
-      <div>
-        <Navbar/>
-        <HomePage/>
-        <Page2/>
-        <Page3/>
+    <div>
+       <Navbar/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}>
+              <Route index element={<HomePage />} />
+              <Route path="Page2" element={<Page2 />} />
+              <Route path="Page3" element={<Page3 />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
   )
 }
